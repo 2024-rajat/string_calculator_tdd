3495983104b9edff304f7528830d746e8ee06fb1
2024-rajat <rajat.gupta.sbg@gmail.com>
2025-01-09 17:21:58 +0530

    Added test for handling single valid number input; which is failing because add method is not yet fixed

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe 'StringCalculator' do
         expect(StringCalculator.add('')).to eq(0)
       end
     end
+
+    context 'when the input is a single string' do
+      context 'and the string is a valid number' do
+        it 'returns the number itself' do
+          expect(StringCalculator.add('5')).to eq(5)
+        end
+      end
+    end
   end
 end
