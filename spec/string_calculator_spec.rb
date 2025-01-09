f8fd60ffc7ce0b41cb67f2fc96dccdaaa12d080f
2024-rajat <rajat.gupta.sbg@gmail.com>
2025-01-09 16:26:11 +0530

    Added initial test for empty string failing due to missing code

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require './lib/string_calculator'
+
+RSpec.describe 'StringCalculator' do
+  describe '.add' do
+    context 'when the input is an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+  end
+end
