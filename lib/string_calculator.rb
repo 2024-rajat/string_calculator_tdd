a84098d098a9f45c2beab1ee1ff5ad481e0fc83c
2024-rajat <rajat.gupta.sbg@gmail.com>
2025-01-09 20:07:15 +0530

    Implemented handling of input with negative numbers in add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,14 +5,22 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    delimiter = ','
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+
+    numbers = numbers.split(delimiter).map(&:to_i)
+    negative_numbers = numbers.select(&:negative?)
+
+    raise "negative numbers not allowed #{negative_numbers.join(', ')}" if negative_numbers.size.positive?
+
+    numbers.sum
+  end
+
+  def self.extract_delimiter_and_numbers(numbers)
     if numbers.start_with?('//')
       delimiter = numbers[2]
-      numbers = numbers[3..]
+      [delimiter, numbers[3..]]
     else
-      numbers = numbers.gsub("\n", ',')
+      [',', numbers.gsub("\n", ',')]
     end
-
-    numbers.split(delimiter).map(&:to_i).sum
   end
 end
