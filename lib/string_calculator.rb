324dd8b9cd0e3d3448a930e50b0d1258ba632d73
2024-rajat <rajat.gupta.sbg@gmail.com>
2025-01-09 16:50:56 +0530

    Implemented handling of empty string in add method

---
+++ lib/string_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# A class for performing calculations based on a provided string of numbers.
+class StringCalculator
+  def self.add(numbers)
+    0 if numbers.empty?
+  end
+end
